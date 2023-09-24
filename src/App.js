0fb46db0b9f8fde17ce84760b36831b776e34d0f
Wesley.Lima <wesleyaz0909@gmail.com>
2023-09-23 23:18:42 -0300

    inicio do projeto e evolucao das telas de consulta

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './components/home';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <Home />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/home/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import '../../App.css';
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Firma</h1>
+      </header>
+      <button className="App-body">
+        setor
+      </button>
+    </div>
+  )
+}
+export default Home;
